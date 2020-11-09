d46782c9673f15f6ad22443e4c65d196906416b4
Alexis-Pel <alexispelissier2002@gmail.com>
2020-11-09 14:58:03 +0100

    squelette proie et prédateur

--- src/com/company/Predator.java
+++ src/com/company/Predator.java
@@ -1,4 +1,20 @@
 package com.company;
 
 public class Predator {
+    static final int AgeMaxAll = 40;
+    int Age;
+    int AgeMax;
+
+    static final int NbLifePointsMaxAll = 120;
+    int NbLifePoints;
+    int NbLifePointsMax;
+
+    Smelling smelling;
+    Vision vision;
+
+    public void hunt(){}
+    public void mate(){}
+    public void eat(){}
+    public void drink(){}
+    public void move(){}
 }
--- src/com/company/Prey.java
+++ src/com/company/Prey.java
@@ -1,4 +1,20 @@
 package com.company;
 
 public class Prey {
+    static final int AgeMaxAll = 30;
+    int Age;
+    int AgeMax;
+
+    static final int NbLifePointsMaxAll = 70;
+    int NbLifePoints;
+    int NbLifePointsMax;
+
+    Hearing hearing;
+    Vision vision;
+
+    public void flee(){}
+    public void mate(){}
+    public void eat(){}
+    public void drink(){}
+    public void move(){}
 }
